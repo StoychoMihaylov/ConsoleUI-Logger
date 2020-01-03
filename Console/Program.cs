58da46a74e1ba810a2fb25340d1f59e9914c99bb
StoychoMihaylov <st.mihaylov.mihaylov@gmail.com>
2020-01-03 19:59:45 +0100

    first commint

---
+++ Console/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleUI
+{
+    using System;
+    using System.Net;
+    using System.IO;
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            TakveValuesAsync();
+        }
+
+        private static void TakveValuesAsync()
+        {
+            string url = @"https://localhost:44323/api/values";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                var content = reader.ReadToEnd();
+
+                var result = JsonConvert.DeserializeObject<IList<dynamic>>(content);
+
+                foreach(var item in result)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
